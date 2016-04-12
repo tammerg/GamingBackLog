e2685a3c27541297c3378a9a91fa4627be73288f
tammerg <tammergalal20@gmail.com>
2016-04-11 20:08:36 -0400

    working more on server.js

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 var express = require('express');
+var PORT = process.env.PORT || 8080;
 
 var app = express();
+
+
+app.listen(PORT, function(){
+  console.log('you are listening on port:', PORT);
+});
